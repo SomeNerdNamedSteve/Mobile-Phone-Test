e0bd85527c0566e41470a01df15ab43994d683df
Steven Burgess <bsteven48@gmail.com>
2018-10-15 12:21:25 -0400

    Init Commit

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    private WebCamTexture mobileCamera;
+
+	// Use this for initialization
+	void Start () {
+        mobileCamera = new WebCamTexture();
+        print(mobileCamera.width);
+        print(mobileCamera.height);
+        //GetComponent<Renderer>().material.mainTexture = mobileCamera;
+        //mobileCamera.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
